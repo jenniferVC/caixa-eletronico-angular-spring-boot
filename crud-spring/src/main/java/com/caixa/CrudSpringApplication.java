7a662471b5bc0fb0123f750c7906f3fd6b2faf4c
JenniferVC <jenniferkt.ifsp@gmail.com>
2022-06-19 17:03:04 -0300

    util

--- crud-spring/src/main/java/com/caixa/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/caixa/CrudSpringApplication.java
@@ -1,9 +1,7 @@
 package com.caixa;
 
 import com.caixa.model.Cliente;
-import com.caixa.model.Course;
 import com.caixa.repository.ClienteRepository;
-import com.caixa.repository.CourseRepository;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,33 +14,5 @@ public class CrudSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
-
-
-  @Bean
-
-  // Será executado assim que a aplicação subir
-  CommandLineRunner initDataBase(CourseRepository courseRepository, ClienteRepository clienteRepository){
-
-    // Função Lambda do Java
-    return args -> {
-
-      // Indicando que é para limpar os dados antes de iniciar
-      // pois dessa forma será inserido novos dados na aplicação
-      courseRepository.deleteAll();
-      clienteRepository.deleteAll();
-
-
-      Course c = new Course();
-      c.setName("Angular com Spring");
-      c.setCategory("front-end");
-
-      Cliente cli = new Cliente();
-      cli.setRg(123);
-      cli.setCpf(456456);
-
-      courseRepository.save(c);
-      clienteRepository.save(cli);
-    };
-  }
 
 }
